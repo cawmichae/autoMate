229fb2390bd0bf7aee69eef6fe84c4955034d93e
cawmichae <chris.michael@sap.com>
2022-10-10 12:55:21 +0100

    Add console logs

--- autoMate/client/automate.js
+++ autoMate/client/automate.js
@@ -1,5 +1,5 @@
-;(function () {
-  console.log('Automate Loaded')
+(function () {
+  console.log('Automate Loaded');
 
   const L = {
     FIRST_PRODUCT: `cx-carousel > .carousel-panel > .slides > .slide.active .active a`,
@@ -22,121 +22,139 @@
     SIGNIN_PASSWORD: `input[formcontrolname="password"]`,
     SIGN_IN_BUTTON: `button[type=submit].btn-primary`,
     SELECT: 'ng-select',
-  }
+  };
 
-  const EMAIL_ADDRESS = `${new Date().getTime()}@test.com`
-  const PASSWORD = 'Password+1234'
+  const EMAIL_ADDRESS = `${new Date().getTime()}@test.com`;
+  const PASSWORD = 'Password+1234';
 
   function waitForElement(selector) {
     return new Promise((resolve) => {
       if (document.querySelector(selector)) {
-        return resolve(document.querySelector(selector))
+        return resolve(document.querySelector(selector));
       }
 
       const observer = new MutationObserver((mutations) => {
         if (document.querySelector(selector)) {
-          resolve(document.querySelector(selector))
-          observer.disconnect()
+          resolve(document.querySelector(selector));
+          observer.disconnect();
         }
-      })
+      });
 
       observer.observe(document.body, {
         childList: true,
         subtree: true,
-      })
-    })
+      });
+    });
   }
 
   {
     waitForElement(L.SAP_ICON_HOME_LINK)
-      .then((element) => waitForElement(L.FIRST_PRODUCT))
       .then((element) => {
-        element.click()
-        return waitForElement(L.PICKUP_OPTIONS_RADIO_PICKUP)
+        console.log('Click on Home Icon');
+        element.click();
+        return waitForElement(L.FIRST_PRODUCT);
       })
       .then((element) => {
-        element.click()
-        return waitForElement(L.USE_MY_LOCATION)
+        console.log('Click on First Product');
+        element.click();
+        return waitForElement(L.PICKUP_OPTIONS_RADIO_PICKUP);
       })
       .then((element) => {
-        element.click()
-        return waitForElement(L.ACTIVE_PICK_UP_IN_STORE_BUTTON)
+        console.log('Click on Pick Up Options radio button');
+        element.click();
+        return waitForElement(L.USE_MY_LOCATION);
       })
       .then((element) => {
-        element.click()
-        return waitForElement(L.ADD_TO_CART)
+        console.log('Click on Use My Location');
+        element.click();
+        return waitForElement(L.ACTIVE_PICK_UP_IN_STORE_BUTTON);
       })
       .then((element) => {
-        element.click()
-        return waitForElement(L.PROCEED_TO_CHECKOUT_BUTTON)
+        console.log('Click on Pick Up In Store radio button');
+        element.click();
+        return waitForElement(L.ADD_TO_CART);
       })
       .then((element) => {
-        element.click()
-        return waitForElement(L.REGISTER_BUTTON)
+        console.log('Click on Add To Cart');
+        element.click();
+        return waitForElement(L.PROCEED_TO_CHECKOUT_BUTTON);
       })
       .then((element) => {
-		console.log('element', element)
-        element.click()
-        return waitForElement(L.FORM_FIRSTNAME)
+        console.log('Click on Proceed To Checkout button');
+        element.click();
+        return waitForElement(L.REGISTER_BUTTON);
       })
       .then((element) => {
-		
-        element.focus()
-        element.value = 'Firstname'
-        element.dispatchEvent(new Event('input'))
-        return waitForElement(L.FORM_LASTNAME)
+        console.log('Click on Register button');
+        element.click();
+        return waitForElement(L.FORM_FIRSTNAME);
       })
       .then((element) => {
-        element.value = 'Lastname'
-        element.dispatchEvent(new Event('input'))
-        return waitForElement(L.FORM_EMAIL)
+        console.log('Set first name and fire input event');
+        element.value = 'Firstname';
+        element.dispatchEvent(new Event('input'));
+        return waitForElement(L.FORM_LASTNAME);
       })
       .then((element) => {
-        element.value = EMAIL_ADDRESS
-        element.dispatchEvent(new Event('input'))
-        return waitForElement(L.FORM_PASSWORD)
+        console.log('Set last name and fire input event');
+        element.value = 'Lastname';
+        element.dispatchEvent(new Event('input'));
+        return waitForElement(L.FORM_EMAIL);
       })
       .then((element) => {
-        element.value = PASSWORD
-        element.dispatchEvent(new Event('input'))
-        return waitForElement(L.FORM_CONFIRM_PASSWORD)
+        console.log('Set email and fire input event');
+        element.value = EMAIL_ADDRESS;
+        element.dispatchEvent(new Event('input'));
+        return waitForElement(L.FORM_PASSWORD);
       })
       .then((element) => {
-        element.value = PASSWORD
-        element.dispatchEvent(new Event('input'))
-        return waitForElement(L.FORM_NEWSLETTER)
+        console.log('Set password and fire input event');
+        element.value = PASSWORD;
+        element.dispatchEvent(new Event('input'));
+        return waitForElement(L.FORM_CONFIRM_PASSWORD);
       })
       .then((element) => {
-        element.click()
-        return waitForElement(L.FORM_TANDC)
+        console.log('Set confirm password and fire input event');
+        element.value = PASSWORD;
+        element.dispatchEvent(new Event('input'));
+        return waitForElement(L.FORM_NEWSLETTER);
       })
       .then((element) => {
-        element.click()
-
-        return waitForElement(L.SUBMIT_REGISTRATION_FORM)
+        console.log('Click Newsletter radio');
+        element.click();
+        return waitForElement(L.FORM_TANDC);
       })
       .then((element) => {
-        element.click()
-        return waitForElement(L.SIGNIN_USERNAME)
+        console.log('Click T and C radio');
+        element.click();
+        return waitForElement(L.SUBMIT_REGISTRATION_FORM);
       })
       .then((element) => {
-        element.value = EMAIL_ADDRESS
-        element.dispatchEvent(new Event('input'))
-        return waitForElement(L.SIGNIN_PASSWORD)
+        console.log('Click Submit button');
+        element.click();
+        return waitForElement(L.SIGNIN_USERNAME);
       })
       .then((element) => {
-        element.value = PASSWORD
-
-        element.dispatchEvent(new Event('input'))
-        return waitForElement(L.SIGN_IN_BUTTON)
+        console.log('Set Username and fire input event');
+        element.value = EMAIL_ADDRESS;
+        element.dispatchEvent(new Event('input'));
+        return waitForElement(L.SIGNIN_PASSWORD);
       })
       .then((element) => {
-        element.click()
-        return waitForElement(L.SELECT)
+        console.log('Set Password and fire input event');
+        element.value = PASSWORD;
+        element.dispatchEvent(new Event('input'));
+        return waitForElement(L.SIGN_IN_BUTTON);
       })
       .then((element) => {
-        element.click()
-        return waitForElement(L.SELECT)
+        console.log('Click Sign In button');
+        element.click();
+        return waitForElement(L.SELECT);
       })
+      .then((element) => {
+        console.log('Click Select');
+        element.click();
+        return waitForElement(L.SELECT);
+      });
   }
-})()
+})();
